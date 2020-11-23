29df8a112426fe042d66f55f58aee97fde5882e8
JessicaThi <jessica.thielemans@hetic.net>
2020-11-23 22:17:18 +0100

    update structure&files + functionality overlay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import React, { useState } from 'react';
 import './App.css';
-import Aside from './components/Aside/Aside';
 import showerIcon from "./assets/images/Shower.png";
 import snowIcon from "./assets/images/Snow.png";
 
 function App() {
+  const [toggleOverlay, setToggleOverlay] = useState(false);
+
+  const closeOverlay = () => {
+    setToggleOverlay(false)
+  }
+
+  const openOverlay = () => {
+    setToggleOverlay(true)
+  }
+
   return (
     <div className="App">
-      <Aside />
+      <div className="aside">
+        <div className={toggleOverlay ? "aside-overlay active" : "aside-overlay"}>
+          <div className="content-overlay">
+            <div>
+              <div onClick={() => closeOverlay()} className='button-close'><svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M6 18L18 6M6 6l12 12"></path></svg></div>
+            </div>
+            <div className="content-search">
+              <input className="input-search" placeholder='search location' />
+              <button className="button-search">Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="content-buttons">
+          <button onClick={() => openOverlay()} className="button">Search for places</button>
+          <button className="button button-round"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg></button>
+        </div>
+        <div className="content-picture-weather-today">
+          <img src={showerIcon} alt="icone weather of the day" />
+        </div>
+        <div className="content-info-weather-today">
+          <p className="temperature">15<span className="measurement">°C</span></p>
+          <p className="weather">Shower</p>
+          <p>Today ・ Fri, 5 Jun</p>
+          <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>Helsinki</p>
+        </div>
+      </div>
       <div className="content">
         <div className="content-center">
           <div className="content-measurement">
@@ -70,7 +105,7 @@ function App() {
             <li className="card-highlights">
               <p>Wind status</p>
               <p className="measurement"><span className="number">7</span> mph</p>
-              <div className="content-wind"><span className="icon-wind"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></span>wsw</div>
+              <div className="content-wind"><span className="icon-wind"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></span>wsw</div>
             </li>
             <li className="card-highlights">
               <p>Humidity</p>
